c405b4d68ae79fb19057225c2726071761cd7290
unknown <hypergamingkudus@gmail.com>
2023-07-12 15:39:35 +0700

    uptade terbaru

--- js/script.js
+++ js/script.js
@@ -13,11 +13,29 @@ hamburger.addEventListener('click', function() {
 window.onscroll = function() {
     const header = document.querySelector('header');
     const FixdNav = header.offsetTop;
+    const toTap = document.querySelector('#to-tap');
 
     if(window.pageYOffset > FixdNav) {
         header.classList.add('navbar-fixed');
+        toTap.classList.remove('hidden');
+        toTap.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed')
+        toTap.classList.remove('flex');
+        toTap.classList.add('hidden');
     }
 
-}
+};
+
+///Click luar hamburger
+
+window.addEventListener('click', function (e) {
+
+    if(e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');   
+    }
+
+});
+
+
